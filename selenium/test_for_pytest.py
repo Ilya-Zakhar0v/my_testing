5373fb5b049841e1b2e68f0896124773da68e29f
Ilya-Zakhar0v <99597582+Ilya-Zakhar0v@users.noreply.github.com>
2022-09-02 00:43:44 +0300

    Update test_for_pytest.py

--- selenium/test_for_pytest.py
+++ selenium/test_for_pytest.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-from selenium.common.exceptions import NoSuchElementException
-
 import time
 
 browser = webdriver.Chrome('/Users/ilyich/Downloads/chromedriver')
